9eaeb67b4d449828be8ea426f70ed3091e97068c
Mantas Pieza <master@Mantass-MacBook-Pro.local>
2024-03-06 12:40:46 +0200

    docstrings written

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -7,12 +7,21 @@ from src.transformer import DataLoader
 
 
 class ProcessOrchestrator:
+    """
+    Class which Orchestrates processes for data extraction and Transformation.
+    """
 
     def __init__(
         self,
         index_range: Set[int] = {i for i in range(1, 58)},
         year_range: Set[int] = {i for i in range(2016, 2023)},
     ) -> None:
+        """_summary_
+
+        Args:
+            index_range (Set[int]): a Set of state indexes (1-57) used for data scarping.. Defaults to {i for i in range(1, 58)}.
+            year_range (Set[int]): a Set of year range (2016-2022) used for data scraping. Defaults to {i for i in range(2016, 2023)}.
+        """
 
         self.index_range = index_range
         self.year_range = year_range
@@ -22,7 +31,18 @@ class ProcessOrchestrator:
         return self._index_range
 
     @index_range.setter
-    def index_range(self, value):
+    def index_range(self, value: Set[int]):
+        """
+        Index_range property setter to validate state indexes.
+
+        Args:
+            value (Set[int]): a Set of state indexes as int
+
+        Raises:
+            Exception: Checks if type of the value is set. Raises TypeError
+            Exception: Checks if all objects in set are int. Raises TypeError
+            Exception: Cheks if range restrictions. Raises ValueError
+        """
         if not isinstance(value, set):
             raise Exception(
                 TypeError(
@@ -32,7 +52,9 @@ class ProcessOrchestrator:
         for i in value:
             if not isinstance(i, int):
                 raise Exception(
-                    "values within index_range set is expected to be integers"
+                    TypeError(
+                        "values within index_range set is expected to be integers"
+                    )
                 )
 
         if min(value) < 1 or max(value) > 57:
@@ -45,7 +67,18 @@ class ProcessOrchestrator:
         return self._year_range
 
     @year_range.setter
-    def year_range(self, value):
+    def year_range(self, value: Set[int]):
+        """
+        year_range property setter to validate year range.
+
+        Args:
+            value (Set[int]): a Set of year_range as int
+
+        Raises:
+            Exception: Checks if type of the value is set. Raises TypeError
+            Exception: Checks if all objects in set are int. Raises TypeError
+            Exception: Cheks if range restrictions. Raises ValueError
+        """
         if not isinstance(value, set):
             raise Exception(
                 TypeError(
@@ -55,7 +88,7 @@ class ProcessOrchestrator:
         for i in value:
             if not isinstance(i, int):
                 raise Exception(
-                    "values within year_range set is expected to be integers"
+                    TypeError("values within year_range set is expected to be integers")
                 )
 
         if min(value) < 2016 or max(value) > 2022:
@@ -64,6 +97,13 @@ class ProcessOrchestrator:
         self._year_range = value
 
     def get_single_state_data(self, year: int, state_code: int) -> None:
+        """
+        Orchestrates extraction of a single page data using DataExtractor class, applies required transformations using DataLoader class.
+
+        Args:
+            year (int): year of the report
+            state_code (int): state code of the report
+        """
 
         state_info = DataExtractor(year=year, state_code=state_code)
         state_info.load_page_to_staging()
@@ -73,6 +113,9 @@ class ProcessOrchestrator:
         DataLoader(year=year, state_name=self.state_name).transform_tables_in_folder()
 
     def get_all_data(self):
+        """
+        Orchestrates extraction of all requested data.
+        """
         if len(self.year_range) > 1 and len(self.index_range) > 1:
             for year in self.year_range:
                 with alive_bar(57) as bar:
--- src/transformer.py
+++ src/transformer.py
@@ -6,12 +6,22 @@ from typing import Set
 
 
 class DataLoader:
+    """
+    Class to load data from staging and transform to parquet file format
+    """
 
     def __init__(
         self,
         year: int = None,
         state_name: str = None,
     ) -> None:
+        """
+        Initiation function for DataLoader
+
+        Args:
+            year (int): year of the report. Defaults to None.
+            state_name (str): name of the state. Defaults to None.
+        """
 
         self.year = year
         self.state_name = state_name
@@ -20,10 +30,23 @@ class DataLoader:
         self.files: Set[str] = {}
 
     def collect_folder_content(self) -> None:
+        """
+        Collects local files from staging folder.
+        """
 
         self.files = set(os.listdir(self.staging_path))
 
-    def read_data(self, file_path, predfined_dtypes) -> pd.DataFrame:
+    def read_data(self, file_path: str, predfined_dtypes: dict) -> pd.DataFrame:
+        """
+        Reads in json file from staging area, applying required datatypes for table
+
+        Args:
+            file_path (str): File path to Json file
+            predfined_dtypes (dict): Contains predefined column data types
+
+        Returns:
+            pd.DataFrame: containing table information from Json file.
+        """
 
         dataframe = pd.read_json(file_path, dtype=predfined_dtypes)
 
@@ -32,6 +55,14 @@ class DataLoader:
     def save_data(
         self, location: str, table_name: str, dataframe: pd.DataFrame
     ) -> None:
+        """
+        Saves table as a .parquet file to a designated location.
+
+        Args:
+            location (str): designated location to save file.
+            table_name (str): filename for table to be saved.
+            dataframe (pd.DataFrame): contains transformed data from staging json file.
+        """
 
         if not os.path.isdir(self.loading_path):
             os.makedirs(self.loading_path)
@@ -39,6 +70,12 @@ class DataLoader:
         dataframe.to_parquet(f"{location}{table_name}.parquet")
 
     def transform_single_table(self, file_name: str) -> None:
+        """
+        Loads single file from staging area, applies transformations and saves it as a parquet file.
+
+        Args:
+            file_name (str): identifies Json file in a folder
+        """
 
         file_path = f"{self.staging_path}{str(file_name)}"
         table_name = file_name[:-5]
@@ -64,6 +101,9 @@ class DataLoader:
         )
 
     def transform_tables_in_folder(self):
+        """
+        Orchestrates all file transformation per folder. removes staging folder after completion.
+        """
 
         self.collect_folder_content()
         for table in self.files:
@@ -73,6 +113,9 @@ class DataLoader:
         self.remove_used_staging_folder()
 
     def remove_used_staging_folder(self):
+        """
+        Removes staging files from local storage after Json files are transformed.
+        """
 
         if os.path.isdir("data/staging/"):
             shutil.rmtree("data/staging/")
